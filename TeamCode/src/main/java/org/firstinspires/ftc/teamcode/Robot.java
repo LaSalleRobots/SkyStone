027b0bc8a06b73dbce391a967c435bd75a13c2e6
Lukas Werner <program4cool@gmail.com>
2019-11-11 11:54:49 -0800

    Improve naming and make setting elapsed time simpler and added movement functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -11,15 +11,18 @@ import com.qualcomm.robotcore.util.Hardware;
 public class Robot {
 
     private HardwareMap hardwareMap;
+    private ElapsedTime runtime;
     private DcMotor leftFront = null;
     private DcMotor rightFront = null;
     private DcMotor leftBack = null;
     private DcMotor rightBack = null;
+    private double power = 0.5;
 
 
     //setup class initalizer
-    public Robot (HardwareMap hardwareMap) {
+    public Robot (HardwareMap hardwareMap, ElapsedTime runtime) {
         this.hardwareMap = hardwareMap;
+        this.runtime = runtime;
 
         //setup motors
         this.leftFront = hardwareMap.get(DcMotor.class, "leftFront");
@@ -36,35 +39,126 @@ public class Robot {
     }
 
 
-    public void rotateLeft (ElapsedTime runtime) {
+    public void rotateLeft () {
         double time = runtime.time();
         double initTime = time;
 
         while (time <= initTime+0.45) {
+            rightBack.setPower(power);
+            rightFront.setPower(power);
+            leftBack.setPower(-power);
+            leftFront.setPower(-power);
+
+            time = runtime.time();
+        }
+
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
+    }
+
+
+    public void rotateRight () {
+        double time = runtime.time();
+        double initTime = time;
+
+        while (time <= initTime+0.45) {
+            leftBack.setPower(power);
+            leftFront.setPower(power);
+            rightBack.setPower(-power);
+            rightFront.setPower(-power);
+
+            time = runtime.time();
+        }
+
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
+    }
+
+    public void moveForward () {
+        double time = runtime.time();
+        double initTime = time;
+
+        while (time <= initTime+0.45) {
+            leftFront.setPower(power);
+            leftBack.setPower(power);
+            rightBack.setPower(power);
+            rightFront.setPower(power);
+            time = runtime.time();
+        }
+
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
+    }
+
+    public void moveBackward () {
+        double time = runtime.time();
+        double initTime = time;
+
+        while (time <= initTime+0.45) {
+            leftFront.setPower(-power);
+            leftBack.setPower(-power);
+            rightBack.setPower(-power);
+            rightFront.setPower(-power);
+            time = runtime.time();
+        }
+
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
+    }
+
+    public void moveRight() {
+        double time = runtime.time();
+        double initTime = time;
+
+        while (time <= initTime+0.45) {
+            leftFront.setPower(power);
+            leftBack.setPower(-power);
+            rightFront.setPower(-power);
             rightBack.setPower(0.5);
-            rightFront.setPower(0.5);
-            leftBack.setPower(-0.5);
-            leftFront.setPower(-0.5);
-
             time = runtime.time();
+
         }
+
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
     }
 
-
-    public void rotateRight (ElapsedTime runtime) {
+    public void moveLeft() {
         double time = runtime.time();
         double initTime = time;
 
         while (time <= initTime+0.45) {
-            leftBack.setPower(0.5);
-            leftFront.setPower(0.5);
-            rightBack.setPower(-0.5);
-            rightFront.setPower(-0.5);
-
+            leftFront.setPower(-power);
+            leftBack.setPower(power);
+            rightFront.setPower(power);
+            rightBack.setPower(-power);
             time = runtime.time();
+
         }
+
+        leftBack.setPower(0);
+        leftFront.setPower(0);
+        rightBack.setPower(0);
+        rightFront.setPower(0);
     }
-    public void sleep(ElapsedTime runtime, double sleepTime) {
+
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+
+
+    public void sleep(double sleepTime) {
         double time = runtime.time();
         double initTime = time;
 
@@ -72,4 +166,5 @@ public class Robot {
             time = runtime.time();
         }
     }
+
 }
