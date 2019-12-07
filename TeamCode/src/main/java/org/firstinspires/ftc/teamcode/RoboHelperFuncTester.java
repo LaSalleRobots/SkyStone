38be42dae4904b78d9894249c1cead847cb52caf
Lukas Werner <program4cool@gmail.com>
2019-12-06 16:20:46 -0800

    Fix stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoboHelperFuncTester.java
@@ -0,0 +1,79 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name="RoboHelperFuncTester")
+public class RoboHelperFuncTester extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private String commands[] = {
+            "moveForwards",
+            "moveBackwards",
+            "moveLeft",
+            "moveRight",
+            "moveBackwardsLeft",
+            "moveBackwardsRight",
+            "moveForwardsLeft",
+            "moveForwardsRight",
+            "rotateLeft",
+            "rotateRight"
+    };
+    private double runTime = 0;
+    private int index = 0;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        telemetry.addData("Status", "Started");
+        telemetry.update();
+
+        RoboHelper robot = new RoboHelper(hardwareMap, runtime);
+
+        while (opModeIsActive()) {
+            if (index > commands.length-1) {index = 0;}
+            else if (index < 0) {index = commands.length - 1;}
+
+
+            if (gamepad1.dpad_down) {
+                index--;
+                robot.sleep(0.25);
+            } else if (gamepad1.dpad_up) {
+                index++;
+                robot.sleep(0.25);
+            } else if (gamepad1.dpad_left) {
+                runTime--;
+                robot.sleep(0.25);
+            } else if (gamepad1.dpad_right) {
+                runTime++;
+                robot.sleep(0.25);
+            } else if (gamepad1.a) {
+                String command = commands[index];
+                if (command.equals("moveForwards")) {robot.moveForwards();}
+                else if (command.equals("moveBackwards")) {robot.moveBackwards();}
+                else if (command.equals("moveLeft")) {robot.moveLeft();}
+                else if (command.equals("moveRight")) {robot.moveRight();}
+                else if (command.equals("moveBackwardsLeft")) {robot.moveBackwardsLeft();}
+                else if (command.equals("moveBackwardsRight")) {robot.moveBackwardsRight();}
+                else if (command.equals("moveForwardsLeft")) {robot.moveForwardsLeft();}
+                else if (command.equals("moveForwardsRight")) {robot.moveForwardsRight();}
+                else if (command.equals("rotateLeft")) {robot.rotateLeft();}
+                else if (command.equals("rotateRight")) {robot.rotateRight();}
+                robot.runFor(runTime);
+            }
+
+            telemetry.addData("Command", commands[index]);
+            telemetry.addData("Index", index);
+            telemetry.addData("Time", runTime);
+            telemetry.update();
+        }
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WallEAutoRight.java
@@ -0,0 +1,114 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+@Autonomous(name="Wall-E Auto Right", group="AI")
+public class WallEAutoRight extends LinearOpMode {
+
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Stone";
+    private static final String LABEL_SECOND_ELEMENT = "Skystone";
+    private static final String VUFORIA_KEY =
+            "ATEunI3/////AAABmTcVGyhDpk/vjQi7sL+G3bkNZLIZNtw/qLByk8N+aFHflTAH7VSKoR4Cmzmgq62zIwg4ijmn/bzp1aCFu3u3S2aGfK0fBzUmddZV8n1+vO2sA4RRALRfeGnv/5UWQHXcqPg1Jz3yysRBhu4ur0g7FadQJq0sTfcoWWsELgQYAeFwsZSl+ktrswOc+SUyhrlJUDJijBL4y2kH4/3aeGYsQhQRVW/0EvSmbPuwMa+6Yo7u1f13PKEOdPWbJYybeGPjybwKrptzgTyNLhSoFqIFiUA8Ft1UD9IalVhGlEsHy6KBcWkvCxGJZBvANancvdTJ0O2Ux2pYsAllxQ+h2oR925ND/oeK5Mgno13LfBKkrm2B";
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+
+    private int sensorWidth = 3264;
+    private double focal = (507 * 60.96) / 20.32; // Precalibrated focal length
+    private int safeZone = 100; // the safezone for getting the bricks centered
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    //Setup Drive Motor variables
+    private DcMotor leftFront = null;
+    private DcMotor rightFront = null;
+    private DcMotor leftBack = null;
+    private DcMotor rightBack = null;
+
+    //Setup Arm Motor variables
+    private DcMotor armLeft = null;
+    private DcMotor armRight = null;
+
+    //Setup claw servos variables
+    private Servo plateGrabber = null;
+    private Servo plateGrabber2 = null;
+    private Servo clawLeft = null;
+    private Servo clawRight = null;
+    private Servo clawRotate = null;
+
+    double leftFrontPower = 0.5;
+    double rightFrontPower = 0.5;
+    double leftBackPower = 0.5;
+    double rightBackPower = 0.5;
+
+    boolean closedMover = false;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initalized");
+        telemetry.update();
+
+
+        //setup motors
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        plateGrabber = hardwareMap.get(Servo.class, "plateGrabber");
+        plateGrabber2 = hardwareMap.get(Servo.class, "plateGrabber2");
+
+        //Set Directions
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        leftBack.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightBack.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+        waitForStart();
+        plateGrabber.setPosition(0);
+        plateGrabber2.setPosition(1);
+        closedMover = true;
+        runtime.reset();
+        RoboHelper robot = new RoboHelper(hardwareMap, runtime);
+
+        if (opModeIsActive()) {
+
+            robot.moveLeft();
+            robot.runFor(1);
+            robot.moveForwards();
+            robot.runFor(2);
+            robot.powerOff();
+            telemetry.addData("Status", "stoping");
+            telemetry.update();
+            robot.togglePlateGrabber();
+            robot.runFor(2);
+            robot.moveBackwards();
+            robot.runFor(2);
+
+
+
+        }
+
+
+
+
+    }
+
+
+
+}
